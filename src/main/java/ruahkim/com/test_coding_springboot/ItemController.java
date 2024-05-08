51512b1c9b062c8172c119c1faed2ce4f7653909
Ruah Kim <kimhk0315@gmail.com>
2024-05-08 14:53:37 +0900

    :art: organization code

--- src/main/java/ruahkim/com/test_coding_springboot/ItemController.java
+++ src/main/java/ruahkim/com/test_coding_springboot/ItemController.java
@@ -1,19 +1,16 @@
 package ruahkim.com.test_coding_springboot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
 
-    private final ItemRepository itemRepository;
     private final ItemService itemService;
 
     @GetMapping("/list")
